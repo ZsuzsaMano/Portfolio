21d5e8b5cc7e651842874cf22f4ae7dad04bca42
Mano <manoaway@gmail.com>
2020-08-04 23:14:34 +0200

    add apps with Handlebar.js

--- projects/app.js
+++ projects/app.js
@@ -1,4 +1,16 @@
-const source   = document.getElementById('text-template').innerHTML;
+//Apps
+
+const appSource   = document.getElementById('app-template').innerHTML;
+const appTemplate = Handlebars.compile(appSource);
+const appContext = { appName: ['Quiz', 'StopWatch', 'Calculator', 'ToDo',
+'ColorGuess', 'Weather', 'Timeline'], };
+const appHtml    = appTemplate(appContext);
+const displayApp = document.getElementById('app');
+displayApp.innerHTML = appHtml;
+
+//Certificates
+
+const source   = document.getElementById('certificate-template').innerHTML;
 const template = Handlebars.compile(source);
 const context = { name: ['Vue', 'CreativeFrontend', 'Bootstrap', 'PHP', 'Git', 'Js', 'debugJs',
 'Nodejs', 'CSS', 'CLI', 'Async'], };
